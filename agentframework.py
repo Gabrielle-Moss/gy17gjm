fa06f49d5c34e5a5973e2088afeeccb2f63f6c20
Gabrielle-Moss <34370949+Gabrielle-Moss@users.noreply.github.com>
2017-12-16 02:57:09 +0000

    Update agentframework.py

--- agentframework.py
+++ agentframework.py
@@ -1,82 +1,114 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 17 12:05:34 2017
+Created on Fri Nov 14 12:05:34 2017
 
 @author: gy17gjm
-"""
-#importing
+
+This formulated code produces the agent based model required to build the agents
+and allow them to move within the envrionment.
+""" 
+
 import random
 
-
-#creating agent class
 class Agent() :   
-#creating random x and y figures for a graph
-   # def __init__(self, environment, agents):
-       #self.agents = agents
-       #self.environment = environment
-       #self.w = len(environment[0])
-       #self.h = len(environment)
-       #self.x = random.randint(0,self.w - 1) 
-      # self.y = random.randint(0,self.h - 1) 
-      
+
+# __init__ = constructor: forming self.envrionment; self.agent; self.x(0) & self.y(0) 
     def __init__(self,environment,agents,x=None,y=None):
+
+# If x=(0), randomise between 0-99, or use available x 
         if (x == None):
-            self.x = random.randint(0,100)
+            self.x = random.randint(0,99)
         else:
             self.x = x
+
+# If y=(0), randomise between 0-100, or use available y 
         if (y == None):
-            self.y = random.randint(0,100)
+            self.y = random.randint(0,99)
         else:
             self.y = y
+            
+"""
+The __init__ function has defined the different aspects of the agent-based model... 
+the environment, agents, x & y; setting both X and Y to none.
+
+By setting x & y to none, or no location, it allows the agent to formulate random
+coordinates between 0-99, however it also allows any provided coordinate to be used.
+
+Creating:
+environment data (.cvs)
+
+y = random y coordinate between 0-99
+x = random x coordinate between 0-99
+else
+y = set y coordinate
+x = set x coordinate
+""" 
+
 
         self.environment = environment 
-        self.agents = agents
-        self.w = len(environment[0])
-        self.h = len(environment)
-        self.x = random.randint(0,self.w - 1) 
-        self.y = random.randint(0,self.h - 1)         
+        self.agents = agents        
         self.store = 0  
 
-            
+"""
+Alters the class name, removing the self. and sets store to zero.
+"""
+
+
     def move(self):
+      
         if random.random() < 0.5:
-            self.x = (self.x +1) % self.w
+            self.x = (self.x + 1) % 100
         else:
-           self.x = (self.x -1) % self.w
+           self.x = (self.x - 1) % 100
            
 
         if random.random() <0.5:
-            self.y = (self.y +1) % self.h
+            self.y = (self.y +1) % 100
         else: 
-            self.y = (self.y -1) % self.h
-            
+            self.y = (self.y -1) % 100
 
-     
-    def talk(self) :
-        self.agents[0].whatever()
-    
+"""
+The move function moves the agent within the formulated environment. The set parameters
+makes the randomly positioned coordinates to move randomly + or - 1 space at a time within
+the environment.
+
+Creating:
+y coordinate moving +/- 1
+x coordinate moving +/- 1
+"""
+
+
+   
     def eat(self): 
+         
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10 
+            
+"""
+This function allows the agents to eat away at the environment DEM. The created function follows:
+If the DEM projects a value of > 10, it should remove a value of 10 leaving zero. But adding
+10 into the self.store created.
+"""
 
 
- #loop the neighbourhood through self.agents
-    def share_with_neighbours(self, neighbourhood):
-        for agent in self.agents:
- #calculate the distance between self and other agents           
-            dist = self.distance_between(agent) 
-        if dist <= neighbourhood:
-#sum self.store & agent.store            
-            sum = self.store + agent.store
-#divide sum by 2 to for average
-            ave = sum /2
-            self.store = ave
-            agent.store = ave
-            print("sharing " + str(dist) + " " + str(ave))
-            
-            
-            
-#pythagoras theory
     def distance_between(self, agent):
         return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
+"""
+distance_between calculates the distance between agents by using pythagoras theorem 
+"""
+
+    def share_with_neighbours(self, neighbourhood):
+      
+        for agent in self.agents:
+            dist = self.distance_between(agent)
+            if dist <=  neighbourhood:
+                total = self.store + agent.store
+                self.store = total / 2 
+                agent.store = total / 2
+"""
+share_with_neighboroughs allows data to be shared between agents. This is conducted within
+the calculcations provided which read...
+If agents are within range of the neighbourhood, total the storage from self. and agent., 
+and split this equally into two stores.
+"""
